5c86a75464fc1a0841744b3285f70344df36548f
r3xq1 <40786755+r3xq1@users.noreply.github.com>
2020-08-17 08:48:12 +0300

    Add files via upload

--- NativeMethods.cs
+++ NativeMethods.cs
@@ -5,9 +5,11 @@
 
     internal static partial class NativeMethods
     {
+        // Для удаления фокуса с элементов 
         [DllImport("user32.dll")]
         internal extern static IntPtr SetFocus(IntPtr hWnd);
 
+        // https://docs.microsoft.com/ru-ru/windows/win32/api/shellapi/nf-shellapi-shgetfileinfoa?redirectedfrom=MSDN
         [DllImport("shell32.dll", CharSet = CharSet.Unicode, EntryPoint = "SHGetFileInfo")]
         public static extern Enums.ExeType GetExeType(string pszPath, uint dwFileAttributes = 0, IntPtr psfi = default, uint cbFileInfo = 0, uint uFlags = 0x2000);
     }
